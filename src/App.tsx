73f74a8b0346d16ab0e2dffdd6173e2a902e17cd
rakuun76 <rakuun76@gmail.com>
2024-07-01 15:42:44 +0900

    기본 레이아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import styled from "styled-components";
+import Board from "./components/Board";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Boards = styled.div`
+  grid-template-columns: repeat(3, 1fr);
+`;
+
 function App() {
-  return <>hello</>;
+  return (
+    <Container>
+      <Boards>
+        <Board />
+        <Board />
+        <Board />
+      </Boards>
+    </Container>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RecoilRoot } from "recoil";
 import GlobalStyle from "./GlobalStyle.tsx";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <RecoilRoot>
+      <GlobalStyle />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
